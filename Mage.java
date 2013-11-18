437f4aba2ff0a14081be3d0ae85ed972cce17702
Ruzeb <ruzeb1996@gmail.com>
2013-11-17 20:07:08 -0500

    Update Mage.java

--- Mage.java
+++ Mage.java
@@ -6,13 +6,13 @@ public class Mage extends Character{
     	_attack = 0.7;
     }
     public Mage(String name) {
-    	this();
-    	_name = name;
+    	super();
+        _strength =30
+        _attack 0.7;
+    	_name = 
+
     }
-    
-    public String getName() {
-    	return _name; 
-    }
+
     public void normalize() {
 		this._defense = 60;
 		this._attack = .6;
