958cd2426923f5ec1df75f159127161c912d4715
emretetik96 <scottetik@gmail.com>
2013-11-18 14:02:04 -0500

    Update Character.java

--- Character.java
+++ Character.java
@@ -3,70 +3,75 @@ public abstract class Character {
 
     //instance variables
     protected String _name;
-	protected int  _health, _strength, _defense;
+        protected int  _health, _strength, _defense, _arrowCtr;
     protected double _attack;
 
     //constructor
     public Character(){
-		_health = 125;
-		_strength = 100;
-		_defense = 40;
-		_attack = 0.4;
+                _health = 125;
+                _strength = 100;
+                _defense = 40;
+                _attack = 0.4;
+                _arrowCtr = 10;
     }
 
     //checks to see if character is still alive
     public boolean isAlive(){
-		boolean result = false;
-		if (_health > 0)
-		    result = true;
-		else
-		    result = false;
-		return result;
+                boolean result = false;
+                if (_health > 0)
+                    result = true;
+                else
+                    result = false;
+                return result;
     }
     
     //attack
     public int attack(Character type){
-    	int damage = 0;
-    	damage = (int)(this._strength * this._attack) - type.getDefense();
-    	if (damage < 0)
-       		damage = 0;
-    	type.lowerHP(damage);
-	    return damage;
+            int damage = 0;
+            damage = (int)(this._strength * this._attack) - type.getDefense();
+            if (damage < 0)
+                       damage = 0;
+            type.lowerHP(damage);
+            return damage;
     }
 
     //Accessors
     public int getDefense(){
-    	return _defense;
+            return _defense;
     }
     
     public String getName(){
-    	return _name;
+            return _name;
     }
 
     //lowers the health by input value
     public void lowerHP(int point){
-    	_health -= point;
+            _health -= point;
     }
     
     
     //increases character's attack by 1 and decreases defense by 10
     public void specialize(){
-    	_defense -= 10;
-    	_attack += 1;
+            _defense -= 10;
+            _attack += 1;
     }
     
     //reverts stats for defense and attack back to normal
-    //abstract because each character type normalizes differently
     public abstract void normalize();
     
     //heals a Cleric
     public int heal(Cleric med){
-		int hp = 5 + (int)( Math.random() * 20 );
-		if (med.isAlive() && med._health < 30)
-			med._health += hp;
-	
-		return hp;
-	}
+                int hp = 5 + (int)( Math.random() * 20 );
+                if (med.isAlive() && med._health < 30)
+                        med._health += hp;
+        
+                return hp;
+        }
+    
+    //allows an archer to restock on arrows
+    public void restock (Archer shoot) {
+            _arrowCtr = 10;
+    }
     
     //helpers
     public boolean isWarrior(Character test){
@@ -93,37 +98,54 @@ public abstract class Character {
              return false;
     }
     
+    public boolean isArcher(Character test) {
+            Character shoot = new Archer();
+            if (test.getClass() == shoot.getClass()) {
+                    return true;
+            }
+            else {
+                    return false;
+            }
+    
+    }
+    
+    
     //about supplies descriptions about the character
     public static String about(Character player){
-    	Character mon = new Monster();
-    	String mondesc = 
-    			"Found in the lowest rungs of Dante's Inferno, the monster is equipped "+
-    			"with 150 health, random strength between 20 and 65, defense of 20, and attack of 1";
-    			
-    	Character war = new Warrior();
-    	String wardesc = 
-    			"Glorified by the Greek gods, the warrior is equipped with 125 health,"+
-    			"100 strength, 40 defense, and 0.4 attack, but comes with its own specialized power!";
-    	Character heal = new Cleric();
-    	String healdesc = 
-    			"Your savior in battle, the Cleric is able to heal Warriors, but is not"+
-    			"a great fighter. Clerics have 200 health, 20 strength, 130 defense, and .05 attack!";
-    	Character leech = new Necromancer();
-    	String leechdesc = 
-    			"Born under Hades' guardianship, the necromancer is a hero with dark powers."+
-    			"The Necromancer is able to drain the life points of dead enemies!";
-    	if (player.getClass() == mon.getClass())
-    		return mondesc;
-    	else if (player.getClass() == war.getClass())
-    		return wardesc;
-    	else if (player.getClass() == heal.getClass())
-    		return healdesc;
-    	else if (player.getClass() == leech.getClass())
-    		return leechdesc;
-    	else
-    		return "Your character must be new within these territories!";
-    		
+            Character mon = new Monster();
+            String mondesc = 
+                            "Found in the lowest rungs of Dante's Inferno, the monster is equipped "+
+                            "with 150 health, random strength between 20 and 65, defense of 20, and attack of 1";
+                            
+            Character war = new Warrior();
+            String wardesc = 
+                            "Glorified by the Greek gods, the warrior is equipped with 125 health,"+
+                            "100 strength, 40 defense, and 0.4 attack, but comes with its own specialized power!";
+            Character heal = new Cleric();
+            String healdesc = 
+                            "Your savior in battle, the Cleric is able to heal Warriors, but is not"+
+                            "a great fighter. Clerics have 200 health, 20 strength, 130 defense, and .05 attack!";
+            Character leech = new Necromancer();
+            String leechdesc = 
+                            "Born under Hades' guardianship, the necromancer is a hero with dark powers."+
+                            "The Necromancer is able to drain the life points of dead enemies!";
+            Character shoot = new Archer();
+            String shootdesc = 
+                            "The archer's attacks are swift, like the messenger God Hermes, and they are powerful indeed." +
+                        "He has 0.6 attack, but his arrows are limited and must be occasionally restocked." ;           
+            if (player.getClass() == mon.getClass())
+                    return mondesc;
+            else if (player.getClass() == war.getClass())
+                    return wardesc;
+            else if (player.getClass() == heal.getClass())
+                    return healdesc;
+            else if (player.getClass() == leech.getClass())
+                    return leechdesc;
+            else if (player.getClass() == shoot.getClass())
+                    return shootdesc;
+            else
+                    return "Your character must be new within these territories!";
+                    
     }
  
 } //end Character
-
